5b21ceb5f5ca22de05d8655851d4a654680e081f
matachema@yahoo.com <Makinho2109>
2022-01-08 15:08:44 +0200

    done test

--- src/test/java/com/tenniscourts/reservations/ReservationServiceTest.java
+++ src/test/java/com/tenniscourts/reservations/ReservationServiceTest.java
@@ -17,20 +17,84 @@ import java.time.LocalDateTime;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @SpringBootTest
 @RunWith(MockitoJUnitRunner.class)
-@ContextConfiguration(classes = ReservationService.class)
+@ContextConfiguration(classes = RefundComponent.class)
 public class ReservationServiceTest {
 
+    private static BigDecimal DEPOSIT  = new BigDecimal(10L);
+
     @InjectMocks
-    ReservationService reservationService;
+    RefundComponent refundComponent;
 
     @Test
     public void getRefundValueFullRefund() {
+        final BigDecimal expected = DEPOSIT;
+
         Schedule schedule = new Schedule();
+        schedule.setStartDateTime(LocalDateTime.now().plusDays(2));
 
-        LocalDateTime startDateTime = LocalDateTime.now().plusDays(2);
+        BigDecimal resp = refundComponent.getRefundValueByReservationStatus(initReservation(schedule, ReservationStatus.READY_TO_PLAY), ReservationStatus.COMPLETED);
+        Assert.assertEquals(resp, expected);
+    }
 
-        schedule.setStartDateTime(startDateTime);
+    @Test
+    public void getRefundValue0Refund() {
 
-        Assert.assertEquals(reservationService.getRefundValue(Reservation.builder().schedule(schedule).value(new BigDecimal(10L)).build()), new BigDecimal(10));
+        final BigDecimal expected = BigDecimal.valueOf(0);
+
+        Schedule schedule = new Schedule();
+        schedule.setStartDateTime(LocalDateTime.now().plusDays(2));
+
+        BigDecimal resp = refundComponent.getRefundValueByReservationStatus(initReservation(schedule, ReservationStatus.READY_TO_PLAY), ReservationStatus.NO_SHOW);
+        Assert.assertEquals(resp, expected);
+    }
+
+    @Test
+    public void getRefundValue25Refund() {
+
+        final BigDecimal expected = (DEPOSIT).multiply(BigDecimal.valueOf(0.25));
+
+        Schedule schedule = new Schedule();
+        schedule.setStartDateTime(LocalDateTime.now().plusHours(1));
+
+        BigDecimal respCancelled = refundComponent.getRefundValueByReservationStatus(initReservation(schedule, ReservationStatus.READY_TO_PLAY), ReservationStatus.CANCELLED);
+        Assert.assertEquals(respCancelled, expected);
+        BigDecimal respRescheduled = refundComponent.getRefundValueByReservationStatus(initReservation(schedule, ReservationStatus.READY_TO_PLAY), ReservationStatus.RESCHEDULED);
+        Assert.assertEquals(respRescheduled, expected);
+    }
+
+    @Test
+    public void getRefundValue50Refund() {
+
+        final BigDecimal expected = (DEPOSIT).multiply(BigDecimal.valueOf(0.50));
+
+        Schedule schedule = new Schedule();
+        schedule.setStartDateTime(LocalDateTime.now().plusHours(3));
+
+        BigDecimal respCancelled = refundComponent.getRefundValueByReservationStatus(initReservation(schedule, ReservationStatus.READY_TO_PLAY), ReservationStatus.CANCELLED);
+        Assert.assertEquals(respCancelled, expected);
+        BigDecimal respRescheduled = refundComponent.getRefundValueByReservationStatus(initReservation(schedule, ReservationStatus.READY_TO_PLAY), ReservationStatus.RESCHEDULED);
+        Assert.assertEquals(respRescheduled, expected);
+    }
+
+    @Test
+    public void getRefundValue75Refund() {
+
+        final BigDecimal expected = (DEPOSIT).multiply(BigDecimal.valueOf(0.75));
+
+        Schedule schedule = new Schedule();
+        schedule.setStartDateTime(LocalDateTime.now().plusHours(13));
+
+        BigDecimal respCancelled = refundComponent.getRefundValueByReservationStatus(initReservation(schedule, ReservationStatus.READY_TO_PLAY), ReservationStatus.CANCELLED);
+        Assert.assertEquals(respCancelled, expected);
+        BigDecimal respRescheduled = refundComponent.getRefundValueByReservationStatus(initReservation(schedule, ReservationStatus.READY_TO_PLAY), ReservationStatus.RESCHEDULED);
+        Assert.assertEquals(respRescheduled, expected);
+    }
+
+    private Reservation initReservation(Schedule schedule, ReservationStatus reservationStatus){
+        return Reservation.builder().
+                schedule(schedule)
+                .value(DEPOSIT)
+                .reservationStatus(reservationStatus)
+                .build();
     }
 }
